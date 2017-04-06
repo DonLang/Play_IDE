e4334fe86f7f4bd665c9294bb206f189d80c8874
DonLang <donaldnlang2@gmail.com>
2017-04-06 15:53:03 -0700

    Clean up code

--- app/assets/javascripts/ide.js
+++ app/assets/javascripts/ide.js
@@ -1,6 +1,9 @@
 
 $( document ).ready(function() {
-   setupEditor()
+
+   setupEditor();
+   setupNavbar();
+
  });
 
 
@@ -10,20 +13,25 @@ function setupEditor(){
     editor.setTheme("ace/theme/twilight");
     editor.getSession().setMode("ace/mode/html");
     editor.setValue('<h2> The world is Great</h2>')
+    setupButton(editor)
 
 
+}
+
+function setupButton(editor){
   $(":button").click(function(response){
     var codeToRender = editor.getValue();
     console.log(codeToRender)
     var $iframe = $('#renderbox');
     $iframe.ready(function() {
     $iframe.contents().find("body").empty().append(codeToRender);
+    });
   });
+}
 
-  });
-
+function setupNavbar(){
   $(".languages").click(function(event){
-      event.preventDefault()
+      event.preventDefault();
 
   });
 }
