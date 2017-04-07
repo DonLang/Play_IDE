59bc7557fcc4e8aaa463cf24d98852a064b3f9bd
DonLang <donaldnlang2@gmail.com>
2017-04-07 15:48:43 -0700

    Add basic CSS functionality

--- app/assets/javascripts/ide.js
+++ app/assets/javascripts/ide.js
@@ -13,17 +13,22 @@ function setupEditor(){
     html_editor.setTheme("ace/theme/twilight");
     html_editor.getSession().setMode("ace/mode/html");
     html_editor.setValue('<h2> The world is Great</h2>')
-    setupButton(html_editor)
+    setupButton(html_editor);
   var css_editor = ace.edit("css_editor");
     css_editor.setTheme("ace/theme/twilight");
     css_editor.getSession().setMode("ace/mode/css");
-    css_editor.setValue('')
+    css_editor.setValue('');
     // setupButton(css_editor)
+    $(":button").click(function(response){
+    var css_result = css_editor.getValue();
+    var $iframe = $('#renderbox');
+    $iframe.contents().find("head").empty().append("<style>" +css_result+"</style>");
+  });
 
 var javascript_editor = ace.edit("javascript_editor");
     javascript_editor.setTheme("ace/theme/twilight");
     javascript_editor.getSession().setMode("ace/mode/javascript");
-    javascript_editor.setValue('')
+    javascript_editor.setValue('');
     // setupButton(javascript_editor)
 }
 
@@ -33,7 +38,6 @@ var javascript_editor = ace.edit("javascript_editor");
 function setupButton(editor){
   $(":button").click(function(response){
     var codeToRender = editor.getValue();
-    console.log(codeToRender)
     var $iframe = $('#renderbox');
     $iframe.ready(function() {
     $iframe.contents().find("body").empty().append(codeToRender);
@@ -45,9 +49,7 @@ function setupNavbar(){
   $(".languages").click(function(event){
       event.preventDefault();
      $(this).parents().addClass('active').siblings().removeClass('active');
-     results = $($(this)[0]).html()
-     console.log(results)
-     $("#"+results.toLowerCase()+"_editor").show().siblings().hide()
-
-  });
+     results = $($(this)[0]).html();
+     $("#"+results.toLowerCase()+"_editor").show().siblings().hide();
+});
 }
