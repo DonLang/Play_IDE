f313cb06e5c17394c81b36fcd8c07754f44f1ec6
DonLang <donaldnlang2@gmail.com>
2017-04-07 11:35:39 -0700

    Add additional code editors

--- app/assets/javascripts/ide.js
+++ app/assets/javascripts/ide.js
@@ -9,14 +9,28 @@ $( document ).ready(function() {
 
 function setupEditor(){
   "use strict"
-  var editor = ace.edit("editor");
+  var editor = ace.edit("html_box");
     editor.setTheme("ace/theme/twilight");
     editor.getSession().setMode("ace/mode/html");
     editor.setValue('<h2> The world is Great</h2>')
     setupButton(editor)
+  var editor = ace.edit("css_box");
+    editor.setTheme("ace/theme/twilight");
+    editor.getSession().setMode("ace/mode/css");
+    editor.setValue('')
+    setupButton(editor)
+
+var editor = ace.edit("javascript_box");
+    editor.setTheme("ace/theme/twilight");
+    editor.getSession().setMode("ace/mode/javascript");
+    editor.setValue('')
+    setupButton(editor)
 
 
 }
+
+
+
 
 function setupButton(editor){
   $(":button").click(function(response){
@@ -32,8 +46,7 @@ function setupButton(editor){
 function setupNavbar(){
   $(".languages").click(function(event){
       event.preventDefault();
-     console.log(this)
-     console.log($(this).parents().addClass('active').siblings().removeClass('active'))
+     $(this).parents().addClass('active').siblings().removeClass('active')
 
   });
 }
