813415fab359d70460258f24d88da909fa281e17
DonLang <donaldnlang2@gmail.com>
2017-04-11 07:53:49 -0700

    Fix associations

--- db/migrate/20170410182001_create_csscodes.rb
+++ db/migrate/20170410182001_create_csscodes.rb
@@ -2,6 +2,7 @@ class CreateCsscodes < ActiveRecord::Migration[5.0]
   def change
     create_table :csscodes do |t|
       t.string :code
+      t.belongs_to :page, index: true
 
       t.timestamps
     end
